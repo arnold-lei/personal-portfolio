f906d9d64107514e345caeef1049f4d9ac61ac49
Arnold Lei <arnold.lei88@gmail.com>
2017-01-16 23:52:49 -0500

    working on contact form

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ require('dotenv').config({
 //App middleware -------------------------------------------/
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({
-  extended: false
+  extended: true
 }));
 app.use(bodyParser.json());
 app.use('/', express.static(__dirname + '/public'));
@@ -28,6 +28,37 @@ app.set('view engine', 'handlebars');
 // var apiRoutes = require('./controllers/routes/apiRoutes')(app);
 
 app.get('/', function(req, res){
+    res.render('landing');
+})
+
+app.post("/contact", function(req, res){
+    console.log("name: ", req.body.name)
+    console.log("email: ", req.body.email)
+    console.log( "message: ", req.body.message)
+    var name = req.body.name
+    var email = req.body.email
+    var form_subject = req.body.subject
+    var msg = req.body.msg
+    var helper = require('sendgrid').mail
+
+    from_email = new helper.Email("test@example.com")
+    to_email = new helper.Email(email)
+    subject = form_subject
+    content = new helper.Content(msg)
+    mail = new helper.Mail(from_email, subject, to_email, content)
+
+    var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+    var request = sg.emptyRequest({
+      method: 'POST',
+      path: '/v3/mail/send',
+      body: mail.toJSON()
+    });
+
+    sg.API(request, function(error, response) {
+      console.log(response.statusCode)
+      console.log(response.body)
+      console.log(response.headers)
+    })
     res.render('landing');
 })
 
